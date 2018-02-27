1ed2fb0057a86a73b17d85e6d9be3cafa3493f28
Eric Banker <ebanker@gmail.com>
2018-02-27 16:59:00 -0500

    just a little cleanup

--- DomeController/DomeController.ino
+++ DomeController/DomeController.ino
@@ -1,34 +1,26 @@
 #define dirOut 2
 #define pwmOut 3
-#define TransIn 4
-
-int ch1 = 0;  // Receiver Channel 1 PPM value
+#define transIn 4
 
 void setup() {
   pinMode(dirOut,OUTPUT);
   pinMode(pwmOut,OUTPUT);
-  pinMode(TransIn, INPUT);
-  Serial.begin(9600);
+  pinMode(transIn, INPUT);
 }
 
 void loop() {
   // Read in the length of the signal in microseconds
-  ch1 = pulseIn(TransIn, HIGH,25000);
+  int ch1 = pulseIn(transIn, HIGH,25000);
   int inputVal = map(ch1, 1000, 2000, -500, 500);
   int pwmVal = 0;
-  int directon = 0;
   
   if( inputVal > 0 ){
     // forward
-    directon = 0;
     pwmVal = map(inputVal, 0, 500, 0, 255);
-
     digitalWrite(dirOut,HIGH);
   }else{
     // backward
-    directon = 1;
     pwmVal = map(inputVal, 0, -500, 0, 255);
-
     digitalWrite(dirOut,LOW);
   }
 
@@ -43,12 +35,6 @@ void loop() {
 
   // set the motor speed 
   analogWrite(pwmOut, pwmVal);
-
-  Serial.print("Direction: ");
-  Serial.print(directon);
-  Serial.print(" - Speed: ");
-  Serial.println(pwmVal);
-  Serial.println("------------");
   
   delay(100);
 }
